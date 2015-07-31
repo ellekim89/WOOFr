f56c791da45742bb0c9788c2fa9bbf7f71544cf2
ellekim89 <ellekim89@gmail.com>
2015-07-30 23:55:43 -0700

    styled

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,8 +8,7 @@ class SessionsController < ApplicationController
     user = User.authenticate(params[:user][:email], params[:user][:password])
     if(user)
       session[:user_id] = user.id
-      flash[:success] = "You are logged in."
-      redirect_to search_path
+      redirect_to root_path
     else
       flash[:danger] = "Invalid email or password."
       redirect_to login_path
